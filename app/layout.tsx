bc1fec56ab9681a89f198c06b5df8cbc39e9ec8f
unknown <arthur.theodoro.sva@hotmail.com>
2023-12-09 19:18:12 -0300

    Hero section done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanva from "@/components/main/StarBackground";
+import NavBar from "@/components/main/NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Arthur Theodoro',
-  description: 'This is my portfolio',
-}
+  title: "Arthur Theodoro",
+  description: "This is my portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body
+        className={`${inter.className} bg-[#030014] h-full overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanva />
+        <NavBar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Hero from "@/components/main/Hero";
 
 export default function Home() {
   return (
-    <main>THIS IS THE PORTFOLIO</main>
-  )
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20 ">
+        <Hero />
+      </div>
+    </main>
+  );
 }
